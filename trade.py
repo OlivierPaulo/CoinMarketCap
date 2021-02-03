80b780d0795854b5130765cd6f2238bc912e8c5e
Olivier Paulo <op.olivierpaulo@gmail.com>
2021-02-03 15:15:35 +0100

    changing round of price to 3 decimals

--- trade.py
+++ trade.py
@@ -40,21 +40,21 @@ def trade_action(latest_trade_price, current_price):
   elif current_price >= latest_trade_price * 1.1:
     return "Sell"
   else:
-    return "Hodl" 
+    return "Hodl"
 
 chat_id = "509161525"
 telegram_token = os.environ.get('TELEGRAM_API_TOKEN')
 
 for ix, coinmarketcap_id in cryptos.coinmarketcap_id.iteritems():
-  cryptos.loc[ix, "current_price"] = round(api_result["data"][str(coinmarketcap_id)]["quote"]["EUR"]["price"],2)
+  cryptos.loc[ix, "current_price"] = round(api_result["data"][str(coinmarketcap_id)]["quote"]["EUR"]["price"],3)
   cryptos.loc[ix, "action"] = trade_action(cryptos.loc[ix, "latest_trade_price"], cryptos.loc[ix, "current_price"])
   if cryptos.loc[ix, "action"] != "Hodl":
     current_price = str(cryptos.loc[ix, 'current_price']).replace('.',',')
     text = f"*Alert {cryptos.loc[ix, 'action']}* `{cryptos.loc[ix, 'name']}` _@ {current_price}€_ per *{cryptos.loc[ix, 'symbol']}*"
     SendMarkdown(chat_id=chat_id, text=text, token=telegram_token)
     cryptos.loc[ix, "latest_trade_price"] = cryptos.loc[ix, "current_price"]
-    cryptos.loc[ix, "buy_price"] = round(cryptos.loc[ix, "latest_trade_price"] / 1.1, 2)
-    cryptos.loc[ix, "sell_price"] = round(cryptos.loc[ix, "latest_trade_price"] * 1.1, 2)
+    cryptos.loc[ix, "buy_price"] = round(cryptos.loc[ix, "latest_trade_price"] / 1.1, 3)
+    cryptos.loc[ix, "sell_price"] = round(cryptos.loc[ix, "latest_trade_price"] * 1.1, 3)
     text2 = f"Next trade for ```{cryptos.loc[ix, 'symbol']}```:\n*Buy* price : _{str(cryptos.loc[ix, 'buy_price']).replace('.',',')}€_ \n*Sell* price : _{str(cryptos.loc[ix, 'sell_price']).replace('.',',')}€_"
     SendMarkdown(chat_id=chat_id, text=text2, token=telegram_token)
 
